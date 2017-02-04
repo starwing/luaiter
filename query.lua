2f5aa565007e02d8fa791e90e68baae05782cccd
Xavier Wang <weasley.wx@gmail.com>
2017-02-04 09:32:29 +0800

    add limit support to recur() remove blacklist (can be implement with filter())

--- query.lua
+++ query.lua
@@ -8,49 +8,18 @@ Query.__index = Query
 
 function Query.new()
    return setmetatable({
-      resolve = function(t, cont) return cont(t, 1) end,
-      blacklist = nil }, Query)
+      resolve = function(t, cont) return cont(t, 1) end
+   }, Query)
 end
 
 function Query:clearpath()
-   for i = 1, #self do
-      self[i] = nil
-   end
-   return self
-end
-
-function Query:recur()
-   local function recur_resolve(t, n, cont)
-      local blacklist = self.blacklist
-      for k, v in pairs(t) do
-         if type(v) == "table" and not blacklist[v] then
-            blacklist[v] = true
-            self[n] = k
-            if cont(v, n+1) then return end
-            recur_resolve(v, n+1, cont)
-         end
-      end
-   end
-   local resolve = self.resolve
-   self.resolve = function(t, cont)
-      return resolve(t, function(v, n)
-         local blacklist = self.blacklist
-         if blacklist then
-            if blacklist[v] then return end
-            blacklist[v] = true
-         end
-         self.blacklist = { [v] = true }
-         if cont(v, n) then return end
-         recur_resolve(v, n, cont)
-         self.blacklist = blacklist
-      end)
-   end
+   for i = 1, #self do self[i] = nil end
    return self
 end
 
 function Query:index(key)
    local resolve = self.resolve
-   self.resolve = function(t, cont)
+   function self.resolve(t, cont)
       return resolve(t, function(v, n)
          local value = v[key]
          if value then self[n] = key; return cont(value, n+1) end
@@ -59,39 +28,9 @@ function Query:index(key)
    return self
 end
 
-function Query:match(patt)
-   local resolve = self.resolve
-   self.resolve = function(t, cont)
-      return resolve(t, function(v, n)
-         for key, value in pairs(v) do
-            if type(key) == "string" and string_match(key, patt) then
-               self[n] = key; return cont(value, n+1)
-            end
-         end
-      end)
-   end
-   return self
-end
-
-function Query:slice(first, last)
-   local resolve = self.resolve
-   self.resolve = function(t, cont)
-      return resolve(t, function(v, n)
-         local cm, cn = first or 1, last or -1
-         if cm < 0 then cm = #v + cm + 1 end
-         if cn < 0 then cn = #v + cn + 1 end
-         for i = cm, cn do
-            local value = v[i]
-            if value then self[n] = i; return cont(value, n+1) end
-         end
-      end)
-   end
-   return self
-end
-
 function Query:map(f)
    local resolve = self.resolve
-   self.resolve = function(t, cont)
+   function self.resolve(t, cont)
       return resolve(t, function(v, n)
          local value, key = f(v)
          self[n] = key or "*mapped*"
@@ -103,7 +42,7 @@ end
 
 function Query:filter(f)
    local resolve = self.resolve
-   self.resolve = function(t, cont)
+   function self.resolve(t, cont)
       return resolve(t, function(v, n)
          if f(v, self, n-1) then return cont(v, n) end
       end)
@@ -111,36 +50,99 @@ function Query:filter(f)
    return self
 end
 
-function Query:all(ty)
+function Query:collect(coll)
    local resolve = self.resolve
-   if not ty then
-      self.resolve = function(t, cont)
-         return resolve(t, function(v, n)
-            for key, value in pairs(v) do
-               self[n] = key; cont(value, n+1)
-            end
-         end)
-      end
-   else
-      self.resolve = function(t, cont)
-         return resolve(t, function(v, n)
-            for key, value in pairs(v) do
-               if type(value) == ty then
-                  self[n] = key; cont(value, n+1)
-               end
-            end
-         end)
-      end
+   function self.resolve(t, cont)
+      return resolve(t, function(v, n)
+         coll[#coll+1] = v
+         return cont(v, n)
+      end)
    end
    return self
 end
 
-function Query:collect(coll)
+function Query:all(ty)
+   if ty then return self:alltype(type) end
    local resolve = self.resolve
-   self.resolve = function(t, cont)
+   function self.resolve(t, cont)
       return resolve(t, function(v, n)
-         coll[#coll+1] = v
-         return cont(v, n)
+         for key, value in pairs(v) do
+            self[n] = key
+            local r = cont(value, n+1); if r then return r end
+         end
+      end)
+   end
+   return self
+end
+
+function Query:alltype(ty)
+   local resolve = self.resolve
+   function self.resolve(t, cont)
+      return resolve(t, function(v, n)
+         for key, value in pairs(v) do
+            if type(key) == ty then
+               self[n] = key
+               local r = cont(value, n+1); if r then return r end
+            end
+         end
+      end)
+   end
+   return self
+end
+
+function Query:match(patt)
+   local resolve = self.resolve
+   function self.resolve(t, cont)
+      return resolve(t, function(v, n)
+         for key, value in pairs(v) do
+            if type(key) == "string" and string_match(key, patt) then
+               self[n] = key
+               local r = cont(value, n+1); if r then return r end
+            end
+         end
+      end)
+   end
+   return self
+end
+
+function Query:recur(limit)
+   limit = limit or -1
+   local function recur_resolve(t, n, deep, cont)
+      for k, v in pairs(t) do
+         if type(v) == "table" then
+            self[n] = k
+            local r = cont(v, n+1); if r then return r end
+            if deep ~= 1 then
+               recur_resolve(v, n+1, deep-1, cont)
+            end
+         end
+      end
+   end
+   local resolve = self.resolve
+   function self.resolve(t, cont)
+      return resolve(t, function(v, n)
+         local r = cont(v, n); if r then return r end
+         recur_resolve(v, n, limit, cont)
+      end)
+   end
+   return self
+end
+
+function Query:slice(first, last, step)
+   first, last, step = first or 1, last or -1, step or 1
+   local resolve = self.resolve
+   function self.resolve(t, cont)
+      return resolve(t, function(v, n)
+         local cm, cn = first, last
+         if cm < 0 then cm = #v + cm + 1 end
+         if cn < 0 then cn = #v + cn + 1 end
+         for i = cm, cn, step do
+            local value = v[i]
+            if value ~= nil then
+               self[n] = i
+               local r = cont(value, n+1); if r then return r end
+            end
+         end
       end)
    end
    return self
